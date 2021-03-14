5bee599a0e42c1c8a72c35a81ee0d39555c173c0
mspil88 <matthewspillane88@gmail.com>
2021-03-14 11:26:14 +0000

    'new_base_case'

--- factorial.py
+++ factorial.py
@@ -1,5 +1,7 @@
 def fact(n):
     '''Function for recursively calculating the factorial of a number''' 
-    if n < 1:
-        return 0
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
     return n*fact(n-1)
